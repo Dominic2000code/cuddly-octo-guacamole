23ef6a01de29cb9717da373e9fdf7055e4a0b990
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2022-11-28 15:29:32 +0000

    modified to display todo items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [todos, setTodos] = useState();
+  // console.log(todos);
+
+  const getTodos = async () => {
+    const res = await axios.get("http://127.0.0.1:8000/api");
+    setTodos(res.data);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <h1> {todo.title} </h1>
+          <span>{todo.body}</span>
+        </div>
+      ))}
     </div>
   );
 }
